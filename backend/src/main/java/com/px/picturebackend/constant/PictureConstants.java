de28312715f87ca5a73c21d0e480efdd7169486d
px <3098263664@qq.com>
2025-08-28 14:46:20 +0800

    docs(constant): 添加PictureConstants空类文件
    添加常量类文件框架，为后续定义图片相关常量做准备

---
+++ backend/src/main/java/com/px/picturebackend/constant/PictureConstants.java
@@ -0,0 +1,13 @@
+package com.px.picturebackend.constant;
+
+/**
+ * packageName: com.px.picturebackend.constant
+ *
+ * @author: idpeng
+ * @version: 1.0
+ * @className: PictureConstants
+ * @date: 2025/8/28 10:30
+ * @description: TODO
+ */
+public class PictureConstants {
+}
